4d8882264f43a7a6e0916553a32a17f5c9c65e27
JAponte29 <jessicakenney87@gmail.com>
2025-05-21 17:59:31 -0700

    Added PaneTwo.xfml and skeleton buttons

--- steganography/src/main/java/com/steganography/SecondaryController.java
+++ steganography/src/main/java/com/steganography/SecondaryController.java
@@ -3,8 +3,28 @@ package com.steganography;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private void onNextClicked() throws IOException {
+        System.out.println("Next button clicked - proceed to next step.");
+        App.setRoot("PaneOne");
+    }
+
+    @FXML
+    private void onSaveClicked() {
+        System.out.println("Save button clicked - message would be saved.");
+    }
+
+    // OLD BUTTONS
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
